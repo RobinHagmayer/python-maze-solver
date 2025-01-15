d9b99b15d504195cad1e16c784577bfc5c2ed3f8
RobinHagmayer <robin.hagmayer@proton.me>
2025-01-15 09:44:10 +0100

    feat: implemented basic window display

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from tkinter import Tk, BOTH, Canvas
 
+
+class Window:
+    def __init__(self, width: int, height: int):
+        self.__root: Tk = Tk()
+        self.__root.title("Python Maze Solver")
+        self.__canvas: Canvas = Canvas(
+            self.__root, bg="white", height=height, width=width
+        )
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running: bool = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def close(self):
+        self.__running = False
+
+
 def main():
-    print("hello")
+    win = Window(800, 600)
+    win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
