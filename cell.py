7aa4855a7d9a8908eeff72616a64949d78addc57
RobinHagmayer <robin.hagmayer@proton.me>
2025-01-15 16:33:53 +0100

    feat: added possibility for win to be none (for testing)

--- cell.py
+++ cell.py
@@ -4,7 +4,7 @@ from graphics import Line, Point, Window
 class Cell:
     def __init__(
         self,
-        win: Window,
+        win: Window | None,
     ) -> None:
         self.has_left_wall: bool = True
         self.has_right_wall: bool = True
@@ -14,9 +14,12 @@ class Cell:
         self._y1: int = -1
         self._x2: int = -1
         self._y2: int = -1
-        self._win: Window = win
+        self._win: Window | None = win
 
     def draw(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        if self._win is None:
+            return
+
         self._x1 = x1
         self._y1 = y1
         self._x2 = x2
@@ -31,12 +34,13 @@ class Cell:
             top: Line = Line(Point(self._x1, self._y1), Point(self._x2, self._y1))
             self._win.draw_line(top)
         if self.has_bottom_wall:
-            bottom: Line = Line(
-                Point(self._x1, self._y2), Point(self._x2, self._y2)
-            )
+            bottom: Line = Line(Point(self._x1, self._y2), Point(self._x2, self._y2))
             self._win.draw_line(bottom)
 
     def draw_move(self, to_cell: "Cell", undo: bool = False) -> None:
+        if self._win is None:
+            return
+
         fill_color: str = "red"
         if undo:
             fill_color = "gray"
--- maze.py
+++ maze.py
@@ -1,10 +1,20 @@
 import time
+
 from cell import Cell
 from graphics import Window
 
 
 class Maze:
-    def __init__(self, x1: int, y1: int, num_rows: int, num_cols: int, cell_size_x: int, cell_size_y: int, win: Window) -> None:
+    def __init__(
+        self,
+        x1: int,
+        y1: int,
+        num_rows: int,
+        num_cols: int,
+        cell_size_x: int,
+        cell_size_y: int,
+        win: Window | None = None,
+    ) -> None:
         self._cells: list[list[Cell]] = []
         self._x1: int = x1
         self._y1: int = y1
@@ -12,7 +22,7 @@ class Maze:
         self._num_cols: int = num_cols
         self._cell_size_x: int = cell_size_x
         self._cell_size_y: int = cell_size_y
-        self._win: Window = win
+        self._win: Window | None = win
 
         self._create_cells()
 
@@ -28,6 +38,8 @@ class Maze:
                 self._draw_cell(col, row)
 
     def _draw_cell(self, col: int, row: int) -> None:
+        if self._win is None:
+            return
         cell: Cell = self._cells[col][row]
         x1: int = self._x1 + col * self._cell_size_x
         y1: int = self._y1 + row * self._cell_size_y
@@ -35,5 +47,7 @@ class Maze:
         self._animate()
 
     def _animate(self) -> None:
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
