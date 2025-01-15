7a50d232874c1dff4f7d0f00e1cdee65f71536f9
RobinHagmayer <robin.hagmayer@proton.me>
2025-01-15 17:09:00 +0100

    test: added test for wall removal of exit and entrance walls

--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 
+from cell import Cell
 from maze import Maze
 
 
@@ -16,6 +17,16 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+
+    def test_maze_entry_and_exit_wall_removal(self) -> None:
+        num_cols: int = 12
+        num_rows: int = 10
+        m1: Maze = Maze(0, 0, num_rows, num_cols, 10, 10)
+        entrance_cell: Cell = m1._cells[0][0]
+        self.assertFalse(entrance_cell.has_top_wall)
+        exit_cell: Cell = m1._cells[num_cols - 1][num_rows - 1]
+        self.assertFalse(exit_cell.has_bottom_wall)
+
 
 if __name__ == "__main__":
     _ = unittest.main()
