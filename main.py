eb8e7f67196b3b08e45f3fc2f8cd3ef688e8bbf3
RobinHagmayer <robin.hagmayer@proton.me>
2025-01-15 10:03:17 +0100

    feat: points, lines and drawing of lines in the window

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from tkinter import Tk, BOTH, Canvas
 
 
+class Point:
+    def __init__(self, x: int, y: int) -> None:
+        self.x: int = x
+        self.y: int = y
+
+
+class Line:
+    def __init__(self, p1: Point, p2: Point) -> None:
+        self.p1: Point = p1
+        self.p2: Point = p2
+
+    def draw(self, canvas: Canvas, fill_color: str) -> None:
+        _ = canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
+
+
 class Window:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int) -> None:
         self.__root: Tk = Tk()
         self.__root.title("Python Maze Solver")
         self.__canvas: Canvas = Canvas(
@@ -12,22 +29,27 @@ class Window:
         self.__running: bool = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw(self):
+    def redraw(self) -> None:
         self.__root.update_idletasks()
         self.__root.update()
 
-    def wait_for_close(self):
+    def wait_for_close(self) -> None:
         self.__running = True
         while self.__running:
             self.redraw()
         print("window closed...")
 
-    def close(self):
+    def close(self) -> None:
         self.__running = False
+
+    def draw_line(self, line: Line, fill_color: str = "black") -> None:
+        line.draw(self.__canvas, fill_color)
 
 
 def main():
     win = Window(800, 600)
+    line: Line = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(line, "blue")
     win.wait_for_close()
 
 
